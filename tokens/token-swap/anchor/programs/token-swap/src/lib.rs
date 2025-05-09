cc789809eaf929449248dcd21656cf3c1d0e2ff2
SkyYap <32660903+SkyYap@users.noreply.github.com>
2025-05-09 23:22:00 +0800

    update to make local deploy successfully
    deploy successfully and all tests works successfully.

--- tokens/token-swap/anchor/programs/token-swap/src/lib.rs
+++ tokens/token-swap/anchor/programs/token-swap/src/lib.rs
@@ -8,7 +8,7 @@ mod instructions;
 mod state;
 
 // Set the correct key here
-declare_id!("AsGVFxWqEn8icRBFQApxJe68x3r9zvfSbmiEzYFATGYn");
+declare_id!("9REFrpSamkX7NYhHVqoDV6ZW9pxRY5Bu5iP684pTPj2X");
 
 #[program]
 pub mod swap_example {
